99e605cb1485c6827dd660d6544d132d37583843
YBoah <yawboahene.yb@gmail.com>
2024-06-06 17:51:15 +0000

    Added an autofocus & inputRef events

--- TREKBAG/src/components/AddItemForm.jsx
+++ TREKBAG/src/components/AddItemForm.jsx
@@ -1,15 +1,16 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Button from "./Button";
 
 function AddItemForm({setItems}) {
   const [itemText, setItemText] = useState("");
-
-  return (
-    <form onSubmit={(e) => {e.preventDefault();
+  const inputRef = useRef();
+  
+  const handleSubmit = (e) => {e.preventDefault();
 
       // basic validation
       if (!itemText) {
         alert("Please enter an item");
+        inputRef.current.focus();
         return;
       }
        
@@ -20,10 +21,16 @@ function AddItemForm({setItems}) {
       }
 
       setItems(prev => [...prev, newItem]);
-    }}>
+
+      setItemText("");
+    }
+
+  return (
+    <form onSubmit={handleSubmit}>
       <h2>Add an item</h2>
-      <input value={itemText} onChange={(e) => setItemText(e.target.value)}
-      autoFocus={true}
+      <input ref={inputRef}
+      value={itemText} onChange={(e) => setItemText(e.target.value)}
+      autoFocus
       />
       <Button>Add to List</Button>
     </form>
