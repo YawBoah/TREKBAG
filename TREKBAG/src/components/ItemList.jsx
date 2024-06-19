6dcd218733c8fab9af46b6eab749357af945e92b
YBoah <yawboahene.yb@gmail.com>
2024-06-19 22:14:08 +0000

    Updating state and naming for props

--- TREKBAG/src/components/ItemList.jsx
+++ TREKBAG/src/components/ItemList.jsx
@@ -5,8 +5,8 @@ function ItemList({ items, handleDeleteItem, handleToggleItem }) {
         <Item
           key={item.id}
           item={item}
-          handleDeleteItem={handleDeleteItem}
-          handleToggleItem={handleToggleItem}
+          onDeleteItem={handleDeleteItem}
+          onToggleItem={handleToggleItem}
         />
       ))}
     </ul>
@@ -15,15 +15,15 @@ function ItemList({ items, handleDeleteItem, handleToggleItem }) {
 
 export default ItemList;
 
-function Item({ item, handleDeleteItem, handleToggleItem }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li className="item">
       <label>
-        <input onChange={() => handleToggleItem(item.id)} checked={item.packed} type="checkbox" />
+        <input onChange={() => onToggleItem(item.id)} checked={item.packed} type="checkbox" />
         {item.name}
       </label>
 
-      <button onClick={() => handleDeleteItem(item.id)}>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
