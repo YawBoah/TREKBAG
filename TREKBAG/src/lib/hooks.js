96071b36e536d0cc24989c636c4abcecd3654fef
YBoah <yawboahene.yb@gmail.com>
2024-07-06 23:16:18 +0000

    Add error handling for useItemsContext hook when used outside ItemsContextProvider
    - Added a check to throw an error if `useItemsContext` is used outside the `ItemsContextProvider`.
    - This ensures that the context is properly initialized before being accessed.

--- TREKBAG/src/lib/hooks.js
+++ TREKBAG/src/lib/hooks.js
@@ -1,9 +1,14 @@
 import { useContext } from "react";
 import { ItemsContext } from "../context/ItemsContextProvider";
 
-
 export function useItemsContext() {
-    const context = useContext(ItemsContext);
-  return context
-}
+  const context = useContext(ItemsContext);
 
+  if (!context) {
+    throw new Error(
+      "useItemsContext must be used within a ItemsContextProvider"
+    );
+  }
+
+  return context;
+}
