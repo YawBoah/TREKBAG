210fc088128667e2c48406ea05b2654332dafc0c
YBoah <yawboahene.yb@gmail.com>
2024-07-08 16:25:09 +0000

    Add ButtonGroup component with item management actions
    - Created ButtonGroup component to handle various item management actions.
    - Integrated useItemsStore for state management functions:
      - markAllAsComplete: Mark all items as complete.
      - markAllAsIncomplete: Mark all items as incomplete.
      - resetToInitial: Reset items to the initial state.
      - removeAllItems: Remove all items from the list.
    - Defined secondaryButtons array to store button configurations.
    - Rendered buttons dynamically using the Button component and mapped through secondaryButtons.

--- TREKBAG/src/components/ButtonGroup.jsx
+++ TREKBAG/src/components/ButtonGroup.jsx
@@ -1,31 +1,43 @@
-import { useContext } from "react";
+import { useItemsStore } from "../stores/itemsStore";
 import Button from "./Button";
-import { ItemsContext } from "../context/ItemsContextProvider";
 
-function ButtonGroup() {
-  const {
-    handleMarkAllAsComplete,
-    handleMarkAllAsIncomplete,
-    handleResetToInitial,
-    handleRemoveAllItems
-  } = useContext(ItemsContext);
+export default function ButtonGroup() {
+  const markAllAsComplete = useItemsStore((state) => state.markAllAsComplete);
+  const markAllAsIncomplete = useItemsStore(
+    (state) => state.markAllAsIncomplete
+  );
+  const resetToInitial = useItemsStore((state) => state.resetToInitial);
+  const removeAllItems = useItemsStore((state) => state.removeAllItems);
+
+  const secondaryButtons = [
+    {
+      text: "Mark all as complete",
+      onClick: markAllAsComplete,
+    },
+    {
+      text: "Mark all as incomplete",
+      onClick: markAllAsIncomplete,
+    },
+    {
+      text: "Reset to initial",
+      onClick: resetToInitial,
+    },
+    {
+      text: "Remove all items",
+      onClick: removeAllItems,
+    },
+  ];
 
   return (
     <section className="button-group">
-      <Button buttonType="secondary" onClick={handleMarkAllAsComplete}>
-        Mark all as complete
-      </Button>
-      <Button buttonType="secondary" onClick={handleMarkAllAsIncomplete}>
-        Mark all as incomplete
-      </Button>
-      <Button buttonType="secondary" onClick={handleResetToInitial}>
-        Reset to initial
-      </Button>
-      <Button buttonType="secondary" onClick={handleRemoveAllItems}>
-        Remove all items
-      </Button>
+      {secondaryButtons.map((button) => (
+        <Button
+          key={button.text + button.onClick.toString()}
+          text={button.text}
+          onClick={button.onClick}
+          buttonType="secondary"
+        />
+      ))}
     </section>
   );
 }
-
-export default ButtonGroup;
